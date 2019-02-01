7cbe84527d596384486b400a37c1a9d079b1b3a1
Michael M <Schnark@users.noreply.github.com>
2019-02-01 10:12:23 +0100

    Fix service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@
 (function (worker) {
 "use strict";
 
-var VERSION = 'v2.2',
+var PREFIX = 'emoticons',
+	VERSION = '2.2',
 	FILES = [
 		'app.js',
 		'index.html',
@@ -11,7 +12,7 @@ var VERSION = 'v2.2',
 
 worker.addEventListener('install', function (e) {
 	e.waitUntil(
-		caches.open(VERSION).then(function (cache) {
+		caches.open(PREFIX + ':' + VERSION).then(function (cache) {
 			return cache.addAll(FILES);
 		})
 	);
@@ -21,7 +22,7 @@ worker.addEventListener('activate', function (e) {
 	e.waitUntil(
 		caches.keys().then(function (keys) {
 			return Promise.all(keys.map(function (key) {
-				if (key !== VERSION) {
+				if (key.indexOf(PREFIX + ':') === 0 && key !== PREFIX + ':' + VERSION) {
 					return caches.delete(key);
 				}
 			}));
